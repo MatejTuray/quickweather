c30e470183654ed17eb529ef58d4defdafc21f4d
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-09-02 05:38:43 +0200

    idk

--- server/build/server.js
+++ server/build/server.js
@@ -3,18 +3,16 @@ const path = require("path");
 const app = new express();
 const http = require('http');
 const server = http.createServer(app);
-const publicPath = path.join(__dirname, "..", "build/");
+const staticFiles = express.static(path.join(__dirname, '/build'))
 require("dotenv").load();
 const port = process.env.PORT || 8080;
 const axios = require("axios");
-
+console.log(staticFiles)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(publicPath));
 require("./api/routes")(app);
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(publicPath, "index.html"));
-});
+app.use('/*', staticFiles)
 server.listen(port, () => {
     console.log("server is up");
 });
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const path = require("path")
 const app = new express()
-const http = require('http');
-const server = http.createServer(app)
-const publicPath = path.join(__dirname, "..", "build/")
+const publicPath = path.join(__dirname, "..", "build")
+console.log(publicPath)
 require("dotenv").load()
 const port = process.env.PORT || 8080;
 const axios = require("axios");
@@ -14,9 +13,9 @@ app.use(express.json());
 app.use(express.static(publicPath))
 require("./api/routes")(app)
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(publicPath, "index.html"))
+    res.sendFile(path.join(publicPath, "index.html"))
 })
-server.listen(port, () => { console.log("server is up") })
+app.listen(port, () => { console.log("server is up") })
 
 
 
