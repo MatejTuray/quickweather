a96dc5aba5b82e5cef2a6729cb997ae573ccef73
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-09-02 10:25:12 +0200

    m

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends React.PureComponent {
 
     }
     componentDidMount() {
-        screen.lockOrientation("portrait")
+        window.screen.lockOrientation("portrait")
         let formSelector = document.querySelector("#options")
         let url
         let forecastType
